e3a7024242c6431288a57054e2ba6df8ae6be92a
alecdragon <83982846+alecdragon@users.noreply.github.com>
2021-05-20 13:48:15 -0500

    volume up

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -61,9 +61,9 @@ ambientSound.player.volume.value = -20; //turn down volume
 export var adventureMusic = new soundFile("obi_v_ani.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -6;
+adventureMusic.player.volume.value = 6;
 
 export var villageMusic = new soundFile("obi_v_ani.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
-villageMusic.player.volume.value = -6;
+villageMusic.player.volume.value = 6;
